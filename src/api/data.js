a220fb4147a69f2223ede0b7e99401c59012d11b
RaffyPetrov <djfanta@abv.bg>
2026-04-21 18:45:14 +0300

    Implemented Question

--- src/api/data.js
+++ src/api/data.js
@@ -1,7 +1,7 @@
 import * as api from './api.js';
 
 
-const host = 'https://parseapi.back4app.com'
+const host = 'https://parseapi.back4app.com';
 api.settings.host = host;
 
 export const login = api.login;
@@ -9,7 +9,38 @@ export const register = api.register;
 export const logout = api.logout;
 
 // Implement application-specific requests
-export async function createQuiz(quiz) {
-    return await api.post(host + '/classes/Quiz', quiz)
+function createPointer(name, id) {
+    return {
+        __type: 'Pointer',
+        className: name,
+        objectId: id 
+    }
 }
-    
+
+function addOwner(object) {
+    const userId = sessionStorage.getItem('userId');
+    object.owner = createPointer('_User', userId);
+
+}
+
+// Quiz Collection
+export async function getQuizes() {
+    return await api.get(host + '/classes/Quiz');
+}
+
+export async function getQuizById(id) {
+    return await api.get(host + '/classes/Quiz/' + id);
+}
+
+export async function createQuiz(quiz) {
+    addOwner(quiz);
+    return await api.post(host + '/classes/Quiz', quiz);
+}
+
+export async function updateQuiz(id, quiz) {
+    return await api.put(host + '/classes/Quiz/' + id, quiz);
+}
+
+export async function deleteQuiz(id) {
+    return await api.del(host + '/classes.Quiz/' + id);
+}
